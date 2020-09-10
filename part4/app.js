776120e6ac5153894a11f98368037fafc1054954
josh-wheeler-arup <jltwheeler@gmail.com>
2020-09-10 21:41:49 +0100

    Submission for exercises 4.1 - 4.2

---
+++ part4/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const blogRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+
+const app = express();
+
+const mongoUrl = config.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) =>
+    logger.error("error connecting to MongoDB: ", error.message)
+  );
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
